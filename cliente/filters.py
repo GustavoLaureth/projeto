6c5db029bec947551972430ecd44989ea91dba04
Gustavo <gustavolaureth22@gmail.com>
2023-02-16 13:40:07 -0300

    update filters and bug/fix

--- cliente/filters.py
+++ cliente/filters.py
@@ -7,4 +7,4 @@ class ClienteFilter(django_filters.FilterSet):
         model = Cliente
         fields = '__all__'
 
-        exclude = ['data_cricao', 'telefone', 'email', 'descricao']
+        exclude = ['data_cricao',]
