8d0f52ee4c3150b76b63ee488221f6a0aee1035f
Gustavo <gustavolaureth22@gmail.com>
2023-02-27 16:06:31 -0300

    updates

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-456+5jcge^juhxrzweo(xix^--f&1!&yk=wb6+w7qp9mal3tdn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
